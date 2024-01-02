cc02401fe85fa73e9a7c14d8ef4420dbaf45703d
JapinanClarence <japinanclarence@gmail.com>
2024-01-02 17:21:11 +0800

    initialized repo

---
+++ php/vendor/composer/installed.php
@@ -0,0 +1,32 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '874c251a0fba4b0e7d0f0acb479c77fbab731b9f',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '874c251a0fba4b0e7d0f0acb479c77fbab731b9f',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'robinvdvleuten/ulid' => array(
+            'pretty_version' => 'v5.0.0',
+            'version' => '5.0.0.0',
+            'reference' => '5389c9a2ff020815cc1f2b840334fdcb84ae3f35',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../robinvdvleuten/ulid',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
